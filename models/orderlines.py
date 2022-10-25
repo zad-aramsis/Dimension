e47c06d13cdba2ea6cff2d490ce2849e406be4a3
zad-aramsis <aramsis@zadsolutions.com>
2022-10-25 10:57:52 +0200

    some updates in views

--- models/orderlines.py
+++ models/orderlines.py
@@ -22,9 +22,7 @@ class SaleOrderLine(models.Model):
 class StockRuleInherit(models.Model):
     _inherit = 'stock.rule'
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, company_id,
-                               values):
-        res = super(StockRuleInherit, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id,
-                                                                   name, origin, company_id, values)
+    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, company_id, values):
+        res = super(StockRuleInherit, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id, name, origin, company_id, values)
         res['dimension'] = values.get('dimension', False)
         return res
